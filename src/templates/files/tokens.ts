c3489b4620ab49ac9caad69b664660fd3173e05a
marcosmarinho <139090070+marcosfrancomarinho@users.noreply.github.com>
2026-08-17 18:23:35 -0300

    add DI tokens template

---
+++ src/templates/files/tokens.ts
@@ -0,0 +1,16 @@
+import { createToken } from './container.js';
+
+/**
+ * Interfaces não existem em tempo de execução. Crie aqui um token
+ * para cada interface que será injetada.
+ *
+ * Exemplo:
+ *
+ * import type { UserRepository } from
+ *   '../domain/user-repository.js';
+ *
+ * export const USER_REPOSITORY =
+ *   createToken<UserRepository>('USER_REPOSITORY');
+ *
+ * Em `providers.ts`, ligue o token à classe que implementa a interface.
+ */
